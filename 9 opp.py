889d183cf705b8aabf153f3b40ecad0831fc3a09
Nirajan Prajapati <142903382+nirajan23@users.noreply.github.com>
2024-10-21 10:36:42 +0300

    update

--- 9 opp.py
+++ 9 opp.py
@@ -1,3 +1,9 @@
 class Dog:
-    pass
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+
+dog= Dog("kalu","street dog")
+dog2=Dog("kali", "Bull dog")
+print(f"name :{dog2.name},\nbreed:{dog.breed}")
 
