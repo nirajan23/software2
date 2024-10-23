e30b53d2312e3c5ae076d45c3db6ff44a21a2e77
Nirajan Prajapati <142903382+nirajan23@users.noreply.github.com>
2024-10-23 09:13:25 +0300

    update

--- 9 opp.py
+++ 9 opp.py
@@ -3,8 +3,20 @@ class Dog:
         self.name = name
         self.breed = breed
 
-dog= Dog("kalu","street dog")
-dog2=Dog("kali", "Bull dog")
+    def bark(self):
+        print(f"{self.name} say Woff!! woff!!")
+    def get_breed(self):
+        return self.breed
+    def set_breed(self,breed):
+        self.breed=breed
+
+dog = Dog("Kalu", "street dog")
+dog2 =Dog("kali",  "Bull dog")
 print(f"name :{dog.name},\nbreed:{dog.breed}")
-print(f"name :{dog.name},\nbreed:{dog.breed}")  
+dog.bark()
+print(f"name :{dog2.name},\nbreed:{dog2.breed}")
+
+kennel = [dog, dog2]
+for dog in kennel:
+    print(f"{dog.name} is kennel")
 
